30a98c79f0f4c63b757c52136ec46046f1207131
Conor Watson <conor.watson94@gmail.com>
2016-12-02 12:44:11 +0000

    Completed Day 2: Parts 1 and 2!

---
+++ Day2/Part1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Part1
+    {
+        static int kWidth = 3; static int kHeight = 3;
+        static int[,] keypad;
+        public static int currentKey = 5;
+        public static List<int> combination = new List<int>();
+
+        public static void createKeypad(int width, int height)
+        {
+            keypad = new int[width, height];
+
+            for(int i = 1; i < width * height + 1; i++)
+            {
+                int x = (i - 1) / width;
+                int y = (i - 1) - (x * height);
+
+                keypad[x, y] = i;
+            }
+        }
+
+        public static void checkBounds(char move)
+        {
+            int tempKey;
+            int ty;
+
+            switch (move)
+            {
+                case 'U':
+                    tempKey = currentKey - kWidth;
+                    if (tempKey > 0 && tempKey < kWidth * kHeight + 1)
+                    {
+                        currentKey = tempKey;
+                    }
+                    break;
+                case 'D':
+                    tempKey = currentKey + kWidth;
+                    if (tempKey > 0 && tempKey < kWidth * kHeight + 1)
+                    {
+                        currentKey = tempKey;
+                    }
+                    break;
+                case 'L':
+                    tempKey = currentKey - 1;
+                    ty = (currentKey - 1) / kWidth;
+                    if (tempKey > ty * kWidth && tempKey < ty * kWidth + kWidth + 1)
+                    {
+                        currentKey = tempKey;
+                    }
+                    break;
+                case 'R':
+                    tempKey = currentKey + 1;
+                    ty = (currentKey - 1) / kWidth;
+                    if (tempKey > ty * kWidth && tempKey < ty * kWidth + kWidth + 1)
+                    {
+                        currentKey = tempKey;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void PartOne()
+        {
+
+            Part1.createKeypad(3, 3);
+
+            foreach (string line in File.ReadLines("../../input.txt"))
+            {
+                foreach (char c in line)
+                {
+                    Part1.checkBounds(c);
+                    Console.WriteLine("Move " + c + " to " + Part1.currentKey);
+                }
+                Part1.combination.Add(Part1.currentKey);
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Combination is: ");
+            foreach (int i in Part1.combination)
+            {
+                Console.Write(i);
+            }
+            Console.Read();
+        }
+
+        static void PartTwo()
+        {
+            Part2.createKeypad();
+
+            foreach (string line in File.ReadLines("../../input.txt"))
+            {
+                foreach (char c in line)
+                {
+                    Part2.checkBounds(c);
+                    Console.WriteLine("Move " + c + " to " + Part2.keypad[Part2.currPos.Item2, Part2.currPos.Item1]);
+                }
+                Part2.combination.Add(Part2.keypad[Part2.currPos.Item2, Part2.currPos.Item1]);
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Combination is: ");
+            foreach (char c in Part2.combination)
+            {
+                Console.Write(c);
+            }
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            //PartOne();
+            PartTwo();
+        }
+    }
+}
---
+++ Day2/Part2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Part2
+    {
+        static int kWidth = 5; static int kHeight = 5;
+        public static char[,] keypad;
+        public static Tuple<int, int> currPos = new Tuple<int, int>(0, 2);
+        public static List<char> combination = new List<char>();
+
+        public static void createKeypad()
+        {
+            keypad = new char[,] { { ' ', ' ', '1', ' ', ' ' },
+                                   { ' ', '2', '3', '4', ' ' },
+                                   { '5', '6', '7', '8', '9' },
+                                   { ' ', 'A', 'B', 'C', ' ' },
+                                   { ' ', ' ', 'D', ' ', ' ' } };
+        }
+
+        public static void checkBounds(char move)
+        {
+            char tempChar;
+            int x = currPos.Item1;
+            int y = currPos.Item2;
+
+            switch (move)
+            {
+                case 'U':
+                    if(x >= 0 && x < kWidth && y - 1 >= 0 && y - 1 < kHeight)
+                    {
+                        tempChar = keypad[x, y - 1];
+                        if (tempChar != ' ')
+                        {
+                            currPos = new Tuple<int, int>(x, y - 1);
+                        }
+                    }
+                    break;
+                case 'D':
+                    if (x >= 0 && x < kWidth && y + 1 >= 0 && y + 1 < kHeight)
+                    {
+                        tempChar = keypad[x, y + 1];
+                        if (tempChar != ' ')
+                        {
+                            currPos = new Tuple<int, int>(x, y + 1);
+                        }
+                    }
+                    break;
+                case 'L':
+                    if (x - 1 >= 0 && x - 1 < kWidth && y >= 0 && y < kHeight)
+                    {
+                        tempChar = keypad[x - 1, y];
+                        if (tempChar != ' ')
+                        {
+                            currPos = new Tuple<int, int>(x - 1, y);
+                        }
+                    }
+                    break;
+                case 'R':
+                    if (x + 1 >= 0 && x + 1 < kWidth && y >= 0 && y < kHeight)
+                    {
+                        tempChar = keypad[x + 1, y];
+                        if (tempChar != ' ')
+                        {
+                            currPos = new Tuple<int, int>(x + 1, y);
+                        }
+                    }
+                    break;
+            }
+        }
+
+    }
+}
