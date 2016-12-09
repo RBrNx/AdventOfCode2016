dbd8b8bd2f3caf79112fa619abd3ae6a4faedeeb
Conor Watson <conor.watson94@gmail.com>
2016-12-09 10:43:03 +0000

    Day 9 Part 1 and 2 Completed! With a little thanks to /r/adventofcode for part 2!

---
+++ Day9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9
+{
+    class Program
+    {
+        static BigInteger decompress(string input, int n)
+        {
+            BigInteger count = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '(')
+                {
+                    string marker = new string(input.Skip(i + 1).TakeWhile(ch => ch != ')').ToArray());
+                    var arr = marker.Split('x');
+                    int nchars = int.Parse(arr[0]);
+                    int skip = i + marker.Length + 2;
+                    count += nchars * int.Parse(arr[1]); //Part 1
+                    //count += decompress(input.Skip(skip).Take(nchars).ToString(), int.Parse(arr[1])); //Part 2
+                    i = skip + nchars - 1;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return count * n;
+        }
+
+        static void Main(string[] args)
+        {
+            string compressed = File.ReadAllText("../../input.txt");
+           
+            Console.WriteLine("The Length of the uncompressed string is: " + decompress(compressed, 1));
+            Console.Read();
+        }
+    }
+}
