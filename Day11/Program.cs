b7a8ba75df7c18f9f48c677810a9fd6e2b88f51d
Conor Watson <conor.watson94@gmail.com>
2016-12-12 17:07:40 +0000

    Started Day 11 and 12

---
+++ Day11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    class Node
+    {
+        public List<Tuple<int, int>> pairs;
+        public int FloorNum;
+        public int distance;
+
+        public Node(List<Tuple<int, int>> p, int fN, int dist)
+        {
+            pairs = p;
+            FloorNum = fN;
+            distance = dist;
+        }
+
+        public Node()
+        {
+
+        }
+    }
+
+    class Program
+    {    
+        static List<Tuple<int, int>> pairs = new List<Tuple<int, int>>() { Tuple.Create(0, 1), Tuple.Create(0, 2) };
+        static HashSet<Node> hash = new HashSet<Node>();
+
+        static void Main(string[] args)
+        {
+            Node startNode = new Node(pairs, 0, 0);
+            int moveCount = 0;
+
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(startNode);
+            hash.Add(startNode);
+
+            while(queue.Count > 0)
+            {
+                //If Floor 4 contains all items - Solved!
+
+                Node current = queue.Dequeue();
+                //Get All (Valid) Successor Nodes
+                //Add them to the queue
+                //Add them to the hash
+            }
+        }
+    }
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    class Program
+    {
+        static Dictionary<string, int> Registers = new Dictionary<string, int>() { { "a", 0 }, { "b", 0 }, { "c", 0 }, { "d", 0 } };
+
+        static void copy(string x, string y)
+        {
+            int n;
+            bool isNum = int.TryParse(x, out n);
+            if (isNum)
+            {
+                Console.WriteLine("Copy " + x + " to " + y);
+                Registers[y] = n;
+            }
+            else
+            {
+                Console.WriteLine("Copy " + x + " to " + y);
+                Registers[y] = Registers[x];
+            }
+        }
+
+        static void increment(string x)
+        {
+            Console.WriteLine("Increment " + x);
+            Registers[x] += 1;
+        }
+
+        static void decrement(string x)
+        {
+            Console.WriteLine("Decrement " + x);
+            Registers[x] -= 1;
+        }
+
+        static int jump(string x, string y)
+        {
+            int n;
+            bool isNum = int.TryParse(x, out n);
+            if (!isNum)
+            {
+                if(Registers[x] != 0)
+                {
+                    return int.Parse(y);
+                }
+                else
+                {
+                    return 1;
+                }             
+            }
+            else
+            {
+                if (n != 0)
+                {
+                    return int.Parse(y);
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            string[] instructions = File.ReadAllLines("../../input.txt");
+
+            for (int i = 0; i < instructions.Length;)
+            {
+                string[] instruc = instructions[i].Split(' ');
+                switch (instruc[0])
+                {
+                    case "cpy":
+                        copy(instruc[1], instruc[2]);
+                        i++;
+                        break;
+                    case "inc":
+                        increment(instruc[1]);
+                        i++;
+                        break;
+                    case "dec":
+                        decrement(instruc[1]);
+                        i++;
+                        break;
+                    case "jnz":
+                        i += jump(instruc[1], instruc[2]);
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("Register A: " + Registers["a"] + "   Register B: " + Registers["b"] + "   Register C: " + Registers["c"] + "   Register D: " + Registers["d"]);
+            Console.Read();
+        }
+    }
+}
