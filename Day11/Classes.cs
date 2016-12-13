9300be8133b8160b2340afbdff4b191deb20abbf
Conor Watson <conor.watson94@gmail.com>
2016-12-13 12:01:39 +0000

    Day 11 Part 1 and 2 FINALLY completed. With thanks to /u/the4ner

---
+++ Day11/Classes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11
+{
+    class Floor
+    {
+        public List<string> Generators { get; set; }
+        public List<string> Microchips { get; set; }
+
+        public List<string> allItems
+        {
+            get
+            {
+                List<string> items = new List<string>();
+                items.AddRange(Generators.OrderBy(x => x));
+                items.AddRange(Microchips.OrderBy(x => x));
+                return items;
+            }
+        }
+
+        public int Pairs
+        {
+            get
+            {
+                int i = 0;
+                foreach (string gen in Generators)
+                {
+                    if (Microchips.Any(c => c.Contains(gen.Split('-').First())))
+                    {
+                        i++;
+                    }
+                }
+                return i;
+            }
+        }
+
+        public int LonelyMicrochips
+        {
+            get
+            {
+                int i = 0;
+                foreach (string micro in Microchips)
+                {
+                    if (!Generators.Any(g => g.Contains(micro.Split('-').First())))
+                    {
+                        i++;
+                    }
+                }
+                return i;
+            }
+        }
+
+        public int LonelyGenerators
+        {
+            get
+            {
+                int i = 0;
+                foreach (string gen in Generators)
+                {
+                    if (!Microchips.Any(g => g.Contains(gen.Split('-').First())))
+                    {
+                        i++;
+                    }
+                }
+                return i;
+            }
+        }
+    }
+
+    class Building
+    {
+        public int Steps { get; set; }
+        public List<Floor> Floors { get; set; }
+        public int currentFloor { get; set; }
+
+        public string State()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < Floors.Count; i++)
+            {
+                sb.Append("Floor " + i);
+                sb.Append(Floors[i].LonelyMicrochips + " " + Floors[i].LonelyGenerators + " " + Floors[i].Pairs);
+            }
+            sb.Append(currentFloor);
+            return sb.ToString();
+        }
+
+        public Building Clone()
+        {
+            Building newBuilding = new Building();
+            newBuilding.Floors = new List<Floor>();
+            for (int i = 0; i < Floors.Count; i++)
+            {
+                newBuilding.Floors.Add(new Floor()
+                {
+                    Microchips = new List<string>(Floors[i].Microchips),
+                    Generators = new List<string>(Floors[i].Generators)
+                });
+            }
+            newBuilding.currentFloor = currentFloor;
+            return newBuilding;
+        }
+
+        public int Goal
+        {
+            get
+            {
+                //Distance from Floor 4 summed
+                int x = 0;
+                for (int f = 0; f < 4; f++)
+                {
+                    for (int i = 0; i < Floors[f].allItems.Count; i++)
+                    {
+                        x += (3 - f);
+                    }
+                }
+                return x;
+            }
+        }
+    }
+}
--- Day11/Program.cs
+++ Day11/Program.cs
@@ -6,48 +6,193 @@ using System.Threading.Tasks;
 
 namespace Day11
 {
-    class Node
-    {
-        public List<Tuple<int, int>> pairs;
-        public int FloorNum;
-        public int distance;
-
-        public Node(List<Tuple<int, int>> p, int fN, int dist)
-        {
-            pairs = p;
-            FloorNum = fN;
-            distance = dist;
-        }
-
-        public Node()
-        {
-
-        }
-    }
-
     class Program
-    {    
-        static List<Tuple<int, int>> pairs = new List<Tuple<int, int>>() { Tuple.Create(0, 1), Tuple.Create(0, 2) };
-        static HashSet<Node> hash = new HashSet<Node>();
+    {
+        static Floor F0 = new Floor()
+        {
+            Microchips = new List<string>(new string[] { "Strontium-M", "Plutonium-M", /*"Elerium-M", "Dilitium-M"*/ }),
+            Generators = new List<string>(new string[] { "Strontium-G", "Plutonium-G", /*"Elerium-G", "Dilitium-G"*/ })
+        };
+        static Floor F1 = new Floor()
+        {
+            Microchips = new List<string>(new string[] { "Ruthenium-M", "Curium-M" }),
+            Generators = new List<string>(new string[] { "Thulium-G", "Ruthenium-G", "Curium-G" })
+        };
+        static Floor F2 = new Floor()
+        {
+            Microchips = new List<string>(),
+            Generators = new List<string>(new string[] { "Thulium-M" })
+        };
+        static Floor F3 = new Floor()
+        {
+            Microchips = new List<string>(),
+            Generators = new List<string>()
+        };
+
+        static HashSet<string> visited = new HashSet<string>();
+        static int solution = int.MaxValue;
+        static Queue<Building> queue = new Queue<Building>();
 
         static void Main(string[] args)
         {
-            Node startNode = new Node(pairs, 0, 0);
-            int moveCount = 0;
+            Building initial = new Building();
+            initial.Floors = new List<Floor>();
+            initial.Floors.Add(F0);
+            initial.Floors.Add(F1);
+            initial.Floors.Add(F2);
+            initial.Floors.Add(F3);
+            initial.currentFloor = 0;
+            initial.Steps = 0;
 
-            Queue<Node> queue = new Queue<Node>();
-            queue.Enqueue(startNode);
-            hash.Add(startNode);
-
+            queue.Enqueue(initial);
+            int count = 0;
+            
             while(queue.Count > 0)
             {
-                //If Floor 4 contains all items - Solved!
+                Building curr = queue.Dequeue();
+                if (visited.Contains(curr.State()))
+                {
+                    continue;
+                }
+                visited.Add(curr.State());
+                count++;
 
-                Node current = queue.Dequeue();
-                //Get All (Valid) Successor Nodes
-                //Add them to the queue
-                //Add them to the hash
+                if (isBuildingComplete(curr))
+                {
+                    if(solution > curr.Steps)
+                    {
+                        solution = curr.Steps;
+                        Console.WriteLine("Shortest Solution Found in : " + solution + " moves");
+                        break;
+                    }
+                }
+
+                List<Building> possibleMoves = getPossibleMoves(curr);
+                for(int i = 0; i < possibleMoves.Count; i++)
+                {
+                    queue.Enqueue(possibleMoves[i]);
+                }
             }
+
+            Console.WriteLine("Finished");
+            Console.Read();
+        }
+        
+        static List<Building> getPossibleMoves(Building b)
+        {
+            List<Building> possMoves = new List<Building>();
+            Floor curr = b.Floors[b.currentFloor];
+            List<Tuple<string, string>> validPairs = getValidPairs(curr);
+
+            for(int i = 0; i < validPairs.Count; i++)
+            {
+                if(b.currentFloor < 3) //Can go up
+                {
+                    Building moved = moveItems(b, validPairs[i], b.currentFloor + 1);
+                    if(moved != null)
+                    {
+                        possMoves.Add(moved);
+                    }
+                }
+                if(b.currentFloor > 0) //Can go up
+                {
+                    Building moved = moveItems(b, validPairs[i], b.currentFloor - 1);
+                    if (moved != null)
+                    {
+                        possMoves.Add(moved);
+                    }
+                }
+            }
+            return possMoves.OrderBy(x => x.Goal).ToList();
+        }
+
+        static Building moveItems(Building b, Tuple<string, string> items, int toFloor)
+        {
+            Building newBuilding = b.Clone();
+            newBuilding.Steps = b.Steps + 1;
+
+            Floor curr = newBuilding.Floors[b.currentFloor];
+            curr.Generators.Remove(items.Item1);
+            curr.Generators.Remove(items.Item2);
+            curr.Microchips.Remove(items.Item1);
+            curr.Microchips.Remove(items.Item2);
+
+            Floor to = newBuilding.Floors[toFloor];
+            if (items.Item1 != null && items.Item1.EndsWith("-G")) //Add First Item to Gens (if valid)
+            {
+                to.Generators.Add(items.Item1);
+            }
+            if (items.Item2 != null && items.Item2.EndsWith("-G")) //Add Second Item to Gens (if valid)
+            {
+                to.Generators.Add(items.Item2);
+            }
+            if (items.Item1 != null && items.Item1.EndsWith("-M")) //Add First Item to Microchips (if valid)
+            {
+                to.Microchips.Add(items.Item1);
+            }
+            if (items.Item2 != null && items.Item2.EndsWith("-M")) //Add Second Item to Microchips (if valid)
+            {
+                to.Microchips.Add(items.Item2);
+            }
+
+            newBuilding.currentFloor = toFloor;
+
+            if(isFloorValid(to) && isFloorValid(curr))
+            {
+                if (visited.Contains(newBuilding.State()))
+                {
+                    return null;
+                }
+                return newBuilding;
+            }
+
+            return null;
+        }
+
+        static List<Tuple<string, string>> getValidPairs(Floor f)
+        {
+            List<Tuple<string, string>> validPairs = new List<Tuple<string, string>>();
+            validPairs.AddRange(f.allItems.SelectMany(x => f.allItems, (x, y) => Tuple.Create(x, y)).Where(tuple => String.Compare(tuple.Item1, tuple.Item2) == -1).Where(x => canMoveTogether(x)));
+            validPairs.AddRange(f.allItems.Select(x => Tuple.Create<string, string>(x, null)));
+            return validPairs;
+        }
+
+        static bool canMoveTogether(Tuple<string, string> itemPair)
+        {
+            string[] item1 = itemPair.Item1.Split('-');
+            string[] item2 = itemPair.Item2.Split('-');
+            return (item1.First() == item2.First()) || (item1.Last() == item2.Last());
+        }
+
+        static bool isBuildingComplete(Building b)
+        {
+            Floor top = b.Floors.Last();
+            if (!isFloorComplete(top))
+            {
+                return false;
+            }
+
+            List<Floor> otherFloors = b.Floors.Take(3).ToList();
+            if(otherFloors.Any(x => !isFloorEmpty(x)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool isFloorEmpty(Floor f)
+        {
+            return f.Microchips.Count == 0 && f.Generators.Count == 0;
+        }
+
+        static bool isFloorComplete(Floor f)
+        {
+            return isFloorValid(f) && f.Microchips.Count > 0 && f.Generators.Count > 0;
+        }
+
+        static bool isFloorValid(Floor f)
+        {
+            return f.Microchips.TrueForAll(c => f.Generators.Contains(c.Replace("-M", "-G"))) || f.Generators.Count == 0;
         }
     }
 }
