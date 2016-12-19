e2252c981a6d2db24e14687ee66a1b65428c9aa4
Conor Watson <conor.watson94@gmail.com>
2016-12-19 12:25:39 +0000

    Day 15 Part 2 complete. Both Parts run in under a second now.

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -9,42 +9,24 @@ namespace Day15
 {
     class Program
     {
-        static int time;
-        static int prevTime = -1;
-        static bool capsuleDropped = false;
-        static int ballPos;
-        static bool running;
-        static List<Tuple<int, int, int>> DiscPositions = new List<Tuple<int, int, int>>();
-        static List<List<Tuple<int, int, int>>> Cache = new List<List<Tuple<int, int, int>>>();
+        static int time = 0;
+        static List<Tuple<int, int>> DiscPositions = new List<Tuple<int, int>>();
 
-        static void calculatePositionsAtTime(int time)
+        static bool isDiscOpen(Tuple<int, int> Disc, int time)
         {
-            if(time < Cache.Count)
+            return (Disc.Item2 + time) % Disc.Item1 == 0;
+        }
+
+        static bool capsuleWillFall(int time)
+        {
+            for(int i = 0; i < DiscPositions.Count; i++)
             {
-                DiscPositions = Cache[time].ToList();
-            }
-            else
-            {
-                for (int i = 0; i < DiscPositions.Count; i++)
+                if(!isDiscOpen(DiscPositions[i], i + time + 1))
                 {
-                    int startingPos = DiscPositions[i].Item2;
-                    int numPos = DiscPositions[i].Item1;
-                    int newPos = startingPos;
-                    for (int j = 0; j < time; j++)
-                    {
-                        if (newPos + 1 < numPos)
-                        {
-                            newPos++;
-                        }
-                        else
-                        {
-                            newPos = 0;
-                        }
-                    }
-                    DiscPositions[i] = Tuple.Create(numPos, startingPos, newPos);
+                    return false;
                 }
-                Cache.Add(DiscPositions.ToList());
-            }            
+            }
+            return true;
         }
 
         static void Main(string[] args)
@@ -53,40 +35,19 @@ namespace Day15
             {
                 string[] values = line.Split(' ');
                 int numPos = int.Parse(values[3]);
-                int currPos = int.Parse(values[11].TrimEnd(new char[] { '.' }));
                 int startingPos = int.Parse(values[11].TrimEnd(new char[] { '.' }));
-                Tuple<int, int, int> disc = Tuple.Create(numPos, startingPos, currPos);
+                Tuple<int, int> disc = Tuple.Create(numPos, startingPos);
                 DiscPositions.Add(disc);
             }
 
-            //DiscPositions.Add(Tuple.Create(11, 0, 0)); //Remove for Part 1
+            DiscPositions.Add(Tuple.Create(11, 0)); //Remove for Part 1
 
-            while (!capsuleDropped)
+            while (!capsuleWillFall(time))
             {
-                time = prevTime + 1;
-                ballPos = -1;
-                calculatePositionsAtTime(time);
-                running = true; //Button Click
-
-                while (running)
-                {
-                    ballPos++;
-                    time++;
-                    calculatePositionsAtTime(time);
-                    if(DiscPositions[ballPos].Item3 != 0)
-                    {
-                        running = false;
-                    }
-                    else if(ballPos == DiscPositions.Count - 1)
-                    {
-                        running = false;
-                        capsuleDropped = true;
-                    }
-                }
-                prevTime++;
+                time++;       
             }
 
-            Console.WriteLine("The First time to press the button is at time = " + (prevTime));
+            Console.WriteLine("The First time to press the button is at time = " + (time));
             Console.Read();
         }
     }
